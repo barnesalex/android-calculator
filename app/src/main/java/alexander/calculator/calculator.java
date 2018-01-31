9769df8229a09782600a976a0ba05c961eedf50e
barnesalex <awbarnes91@gmail.com>
2018-01-30 20:05:04 -0600

    added logic to make buttons display in text

--- app/src/main/java/alexander/calculator/calculator.java
+++ app/src/main/java/alexander/calculator/calculator.java
@@ -2,12 +2,42 @@ package alexander.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class calculator extends AppCompatActivity {
+
+    private TextView _screen;
+    private String display = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        _screen =(TextView)findViewById(R.id.result);
+        _screen.setText(display);
+    }
+
+    protected void updateScreen() {
+        _screen.setText(display);
+    }
+
+    public void onClickNumber (View v) {
+        Button btn = (Button) v;
+        display += btn.getText();
+        updateScreen();
+    }
+
+    public void onClickClear (View v) {
+        Button btn = (Button) v;
+        display = "";
+        updateScreen();
+    }
+
+    public void onClickOp (View v) {
+        Button btn = (Button) v;
+        display += btn.getText();
+        updateScreen();
     }
 }
